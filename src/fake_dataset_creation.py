1744131043742fe1eafa6668c0e094085af4c254
hrumayor@ideko.es <hrumayor@ideko.es>
2024-08-28 12:40:25 +0200

    Final code

--- src/fake_dataset_creation.py
+++ src/fake_dataset_creation.py
@@ -14,22 +14,41 @@ schools = [
     {'name': 'School C', 'address': 'Street C 789', 'zip_code': '34567'},
     {'name': 'School D', 'address': 'Street D 101', 'zip_code': '45678'},
     {'name': 'School E', 'address': 'Street E 202', 'zip_code': '56789'},
+    {'name': 'School F', 'address': 'Street F 123', 'zip_code': '12456'},
+    {'name': 'School G', 'address': 'Street G 456', 'zip_code': '12654'},
+    {'name': 'School H', 'address': 'Street H 789', 'zip_code': '67890'},
+    {'name': 'School I', 'address': 'Street I 101', 'zip_code': '23654'},
+    {'name': 'School J', 'address': 'Street J 202', 'zip_code': '56789'},
+    {'name': 'School K', 'address': 'Street K 789', 'zip_code': '34667'},
+    {'name': 'School L', 'address': 'Street L 101', 'zip_code': '45789'},
+    {'name': 'School M', 'address': 'Street M 202', 'zip_code': '56779'}
 ]
+
+# simulate salary ranges per occupancy
+salary_ranges = {
+    'Engineer': (5000, 20000),
+    'Teacher': (3000, 12000),
+    'Doctor': (15000, 50000),
+    'Lawyer': (10000, 35000),
+    'Nurse': (4000, 14000),
+    'Unemployed': (300, 1568)
+}
 
 # Complete data with simulate values
 data = []
 
 for _ in range(num_students):
     school = random.choice(schools)
+    occupation = random.choice(['Engineer', 'Teacher', 'Doctor', 'Lawyer', 'Nurse', 'Unemployed'])
     student = {
         'First_Name': fake.first_name(),
         'Last_Name': fake.last_name(),
-        'Age': random.randint(6, 18),
-        'School': school['name'],
+        'Age': random.randint(0, 26),
+        'School_Name': school['name'],
         'School_Address': school['address'],
-        'School_zip_code': school['zip_code'],
-        'Parents_salary': random.randint(1150, 100000),
-        'Parents_occupation': random.choice(['Engineer', 'Teacher', 'Doctor', 'Lawyer', 'Nurse', 'Unemployed']),
+        'School_ZipCode': school['zip_code'],         
+        'Parents_Salary': round(random.uniform(*salary_ranges[occupation]), 3),
+        'Parents_Occupation': occupation,
         'Weight': round(random.uniform(20, 80), 1),
         'Size': round(random.uniform(100, 200), 1),
         'Feet_size': random.choice([34, 35, 36, 37, 38, 39, 40, 41, 42]),
